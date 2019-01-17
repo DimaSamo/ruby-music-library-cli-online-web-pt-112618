f66a291c714b7dec346fab62c92823bd647f2c8b
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-17 21:46:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,2 +1,26 @@
 class Artist
+  attr_accessor :name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    new_song = Song.new(name)
+    new_song.save
+    new_song
+  end
+
+  def initialize(name)
+    @name = name
+  end
+
+  def save
+    @@all << self
+  end
 end
