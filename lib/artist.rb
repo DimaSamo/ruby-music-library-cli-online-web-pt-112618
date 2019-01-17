cee7f10fb9f4f7a84a372f748066fb79e89445bf
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-17 21:52:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
 
   def self.all
@@ -19,6 +19,10 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+  end
+
+  def songs
+    @songs
   end
 
   def save
