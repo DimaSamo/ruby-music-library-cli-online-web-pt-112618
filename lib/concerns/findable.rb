81e1ef5764bf1b07683a25d8e1fedbb8711803b3
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-17 22:26:46 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,3 +1,12 @@
 module Concerns::Findable
+  def self.find_by_name(name)
+    self.all.find do |element|
+      element.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
 
 end
